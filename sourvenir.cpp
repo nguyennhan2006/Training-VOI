e248236b2a3232ce529811ce3257aa20f4fdb8b2
nguyennhan2006 <88951683+nguyennhan2006@users.noreply.github.com>
2023-07-27 10:21:14 +0700

    Create sourvenir.cpp

---
+++ sourvenir.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <set>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <map>
+#include <iomanip>
+#include <queue>
+#include <deque>
+#define MT vector<vector<long long>>
+using namespace std;
+const int N = 1e6 + 7, MOD = 1e9 + 19972207;
+
+void feo(){
+    freopen("nhap.inp", "r", stdin);
+    freopen("xuat.out", "w", stdout);
+    freopen("error.err", "w", stderr);
+}
+
+int main(){
+    feo();
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    
+    
+}
